d2cdece10839ed1f11a886b7fe5970aa89c83510
GeoffreyOliver <geoffreyokongo2@gmail.com>
2015-07-31 22:28:24 +0300

    Writing the header file

---
+++ application/views/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head profile="http://www.w3.org/2005/10/profile">
+<link rel="icon" type="image/ico" href="{{Url::assets('img/logo.png')}}">
+
+    <meta charset="UTF-8">
+    <title>Gliver MVC PHP Framework</title>
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,700,600' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+     <!--    LOAD CUSTOM STYLES    -->
+    <link rel="stylesheet" href="{{Url::assets('css/style.css')}}">
+    <link rel="stylesheet" href="{{Url::assets('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{Url::assets('css/dashboard.css')}}">
+    <!-- Optional theme -->
+    
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+
+</head>
