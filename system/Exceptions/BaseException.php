5c1e35891610a7fdea35679423d1f3b9d5bd1e02
GeoffreyOliver <geoffreyokongo2@gmail.com>
2015-07-31 21:37:51 +0300

    Installing the Base MVC Framework for writing the Documentation

---
+++ system/Exceptions/BaseException.php
@@ -0,0 +1,30 @@
+<?php namespace Exceptions;
+
+use Helpers\Path;
+
+class BaseException extends \Exception {
+
+	/**
+	 *This method displays the error message
+	 *
+	 *@param 
+	 *
+	 */
+	public function show()
+	{
+		//get the global $config array for site title
+		global $config;
+
+		//set the title variable
+		$title = $config['title'];
+
+		//the variable to be populated with the error message
+		$msg = $this->getCode() . ': Error on line '.$this->getLine().' in '.$this->getFile() .': <b>  "'.$this->getMessage().' " </b> ';
+
+		//load the template file
+		include Path::sys() . 'Exceptions' . DIRECTORY_SEPARATOR . 'index.php';
+
+	}
+
+	
+}
